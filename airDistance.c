ebd1b714782cc337898a4bd8255c9bee123c9ea4
clarson26 <46770468+clarson26@users.noreply.github.com>
2019-01-23 20:45:09 -0600

    Create airDistance.c

---
+++ airDistance.c
@@ -0,0 +1,26 @@
+/*
+* Cam Larson
+* 1/24/2019
+* Computing the air distance between two giving points on earth
+*/
+
+#include<stdlib.io>
+#include<stdio.h>
+
+  int main()    {
+
+  double longitudeOne;
+  double latitudeOne;
+  double longitudeTwo;
+  double latitudeTwo;
+
+  printf("Current location: (");
+  scanf("%lf", longitudeOne);
+  printf("%lf, ", longitudeOne);
+  scanf("%lf", latitudeOne);
+  printf("%lf)\n", latitudeOne);
+
+
+
+    return 0;
+  }
